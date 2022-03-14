7206e306ed32cbd0765b06ac044a70a34a48114b
Ryan Swart <ryan@talo.systems>
2022-03-14 15:36:49 +0800

    feat: update to latest nimpackage build

--- flake.nix
+++ flake.nix
@@ -3,40 +3,34 @@
     goatnixpkgs.url = "github:TheAncientGoat/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self, nixpkgs, goatnixpkgs, flake-utils
-  }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; };
-    goatpkgs = import goatnixpkgs { inherit system; };
-    nativeBuildInputs = [
-      goatpkgs.arrow-c-glib
-    ];
-    propagatedBuildInputs = [
-      pkgs.glib.out
-      goatpkgs.arrow-c-glib
-    ];
-  in {
-    devShell = with pkgs;
-    mkShell {
-      buildInputs = propagatedBuildInputs ++ [
-        goatpkgs.nim
-        goatpkgs.nrpl
-        goatpkgs.nimlsp
-      ];
-      inherit nativeBuildInputs;
-    };
-    defaultPackage = with pkgs; buildNimPackage rec {
-      pname = "narrow";
-      version = "0.2.0";
-      src = fetchFromGitHub {
-        owner = "talo";
-        repo = pname;
-        rev = version;
-        hash = "sha256-w64ENRyP3mNTtESSt7CDDxUkjYSfziNVVedkO4HIuJ8=";
-      };
-      inherit nativeBuildInputs propagatedBuildInputs;
-    };
-  });
+  outputs = { self, nixpkgs, goatnixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        goatpkgs = import goatnixpkgs { inherit system; };
+        nativeBuildInputs = [ goatpkgs.arrow-c-glib ];
+        propagatedBuildInputs = [ pkgs.glib.out goatpkgs.arrow-c-glib ];
+        devBuildInputs = [ goatpkgs.nim goatpkgs.nrpl goatpkgs.nimlsp ];
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = devBuildInputs ++ propagatedBuildInputs;
+            inherit nativeBuildInputs;
+          };
+        devShells.narrow = pkgs.mkShell {
+          buildInputs = [ self.defaultPackage."${system}" ] ++ devBuildInputs;
+        };
+        defaultPackage = with pkgs;
+          nimPackages.buildNimPackage rec {
+            pname = "narrow";
+            version = "0.2.0";
+            src = fetchFromGitHub {
+              owner = "talo";
+              repo = pname;
+              rev = version;
+              hash = "sha256-w64ENRyP3mNTtESSt7CDDxUkjYSfziNVVedkO4HIuJ8=";
+            };
+            inherit nativeBuildInputs propagatedBuildInputs;
+          };
+      });
 }
