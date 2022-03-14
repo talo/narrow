f47b0b7b297e3536beb357a08670cb28d5f8c7f4
Sean L <sean@talo.systems>
2022-03-14 14:30:45 +0800

    feat: flake package

--- flake.nix
+++ flake.nix
@@ -1,27 +1,42 @@
 {
   inputs = {
-    goatnixpkgs.url = "github:TheAncientGoat/nixpkgs";
+    goatnixpkgs.url = "github:TheAncientGoat/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, goatnixpkgs, flake-utils }:
+  outputs = {
+    self, nixpkgs, goatnixpkgs, flake-utils
+  }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = import nixpkgs { inherit system; };
     goatpkgs = import goatnixpkgs { inherit system; };
+    buildInputs = [
+      pkgs.glib.out
+      goatpkgs.arrow-c-glib
+    ];
+    nativeBuildInputs = [
+      goatpkgs.arrow-c-glib
+    ];
   in {
     devShell = with pkgs;
     mkShell {
-      buildInputs = [
+      buildInputs = buildInputs ++ [
         goatpkgs.nim
         goatpkgs.nrpl
         goatpkgs.nimlsp
-        goatpkgs.arrow-c-glib
       ];
-      nativeBuildInputs = [
-        boost
-        goatpkgs.arrow-c-glib
-      ];
+      inherit nativeBuildInputs;
+    };
+    defaultPackage = with pkgs; buildNimPackage rec {
+      pname = "narrow";
+      version = "0.2.0";
+      src = fetchFromGitHub {
+        owner = "talo";
+        repo = pname;
+        rev = version;
+        hash = "sha256-w64ENRyP3mNTtESSt7CDDxUkjYSfziNVVedkO4HIuJ8=";
+      };
+      inherit buildInputs nativeBuildInputs;
     };
   });
 }
